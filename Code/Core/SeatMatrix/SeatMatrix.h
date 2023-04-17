ab2777b6de3e455e9b696f4ec97171056cfb95e4
Sujeeth13 <72500256+Sujeeth13@users.noreply.github.com>
2023-04-17 17:00:13 +0530

    Create SeatMatrix.h

---
+++ Code/Core/SeatMatrix/SeatMatrix.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+namespace SeatMatrix{
+class SeatMatrix {
+    private: 
+        map<string, map<string,int> > matrix;
+    protected:
+    public:
+        void operations();
+        void checkSystemHealth(shared_ptr<MetaSystemHandler>);
+};
+
+}
