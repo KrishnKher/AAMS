514f317ec1dd083c391d3dcc8e0f77e319c57328
Sujeeth13 <72500256+Sujeeth13@users.noreply.github.com>
2023-04-17 16:59:25 +0530

    Update Project.h

--- Code/Core/Project/Project.h
+++ Code/Core/Project/Project.h
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
 
+namespace Project{
+class Project {
+    private: 
+        Rule currRule;
+        uint currRoundState;
+        vector<string> filePaths;
+        SeatMatrix currSeatMatrix;
+        vector<shared_ptr<UIObservers>> uiObservers; 
+    protected:
+    public:
+        void sort();
+        void checkSystemHealth(shared_ptr<MetaSystemHandler>);
+
+};
+
+}
