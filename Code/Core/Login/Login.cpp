73994921b0ca0c0f0f61952d91e51f33770abbb0
KrishnKher <zetakrh@gmail.com>
2023-04-29 23:43:42 +0530

    Updated Login slightly

--- Code/Core/Login/Login.cpp
+++ Code/Core/Login/Login.cpp
@@ -1,33 +1,32 @@
 #include "Login.h"
 using namespace std;
 
-Login* Login ::loginInstance = NULL;
+Login *Login ::loginInstance = NULL;
 
-bool Login::checkCredentials(string uname, string pwd){
-    fstream logFile("log.json",ios::in | ios::out);
-    string json((istreambuf_iterator<char>(logFile)),istreambuf_iterator<char>());
-    rapidjson::Document doc;
-    bool auth = false;
-    doc.Parse(json.c_str());
-    if (doc.HasParseError()) {
-        cerr << "Error parsing JSON: "
-             << doc.GetParseError() << endl;
-        return 1;
+bool Login::checkCredentials(string userName, string password) {
+  fstream logFile("log.json", ios::in | ios::out);
+  string json((istreambuf_iterator<char>(logFile)), istreambuf_iterator<char>());
+  rapidjson::Document doc;
+  bool auth = false;
+
+  doc.Parse(json.c_str());
+  if (doc.HasParseError()) {
+    cerr << "Error parsing JSON: " << doc.GetParseError() << endl;
+    return 1;
+  }
+
+  for (auto &entry : doc["validUsers"].GetArray()) {
+    if (entry.MemberBegin()->name.GetString() == userName) {
+      if (entry.MemberBegin()->value.GetString() == password) {
+        auth = true;
+      }
+      break;
     }
-    for(auto& x: doc["validUsers"].GetArray()){
-        if(x.MemberBegin()->name.GetString() == uname){
-            if(x.MemberBegin()->value.GetString() == pwd){
-                auth = true;
-                return auth;
-            }
-            else
-                return auth;
-        }
-    }
-    return auth;
+  }
+  return auth;
 }
 
-int main(){
-    Login* l = Login::getInstance();
-    cout<<l->checkCredentials("Sujeth","1234")<<endl;
+int main() {
+  Login *l = Login::getInstance();
+  cout << l->checkCredentials("Sujeth", "1234") << endl;
 }
