4bd7296e583cffa235883af0eed115d75767f757
KrishnKher <72561577+KrishnKher@users.noreply.github.com>
2023-04-29 23:20:22 +0530

    Update Rule.cpp to reflect expected headers

---
+++ Code/Core/Rule/Rule.cpp
@@ -0,0 +1,11 @@
+#ifndef RULE_H
+#define RULE_H
+
+#include <iostream>
+#include <bits/stdc++.h>
+#include "RuleInterface.h"
+using namespace std;
+
+// can have maxbase rule, minbase rule, groupby baserule, etc.
+
+#endif RULE_H
