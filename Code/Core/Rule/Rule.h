36842e9f2c939bf2ff9650a1c9751da8064a6afa
KrishnKher <72561577+KrishnKher@users.noreply.github.com>
2023-04-29 23:20:57 +0530

    Update Rule.h to reflect expected headers

--- Code/Core/Rule/Rule.h
+++ Code/Core/Rule/Rule.h
@@ -3,46 +3,9 @@
 
 #include <iostream>
 #include <bits/stdc++.h>
+#include "RuleInterface.h"
 using namespace std;
 
-namespace Rule{
-
-typedef shared_ptr<BaseRule>, uint ruleElement;
-
-// have to implement approximate factory method here
-
-class Rule{
-private:
-std::map <shared_ptr<BaseRule>, uint> ruleComposition;
-protected:
-friend class BaseRule;
-public:
-Rule(): ruleComposition({}) {};
-Rule(const Rule& referenceRule);
-Rule(Rule&& rule);
-
-bool configureRule();
-bool loadRule(std::map <ruleElement> referenceRuleComposition);
-std::map <ruleElement> fetchRule();
-
-~Rule();
-};
-
-class BaseRule{
-private:
-std::string columnName;
-protected:
-public:
-BaseRule() : columnName("def") {};
-BaseRule(const BaseRule& referenceBaseRule);
-BaseRule(BaseRule&& baseRule);
-
-BaseRule operator < (BaseRule& coBaseRule) {
-
-}
-
-}
-
-}
+// can have maxbase rule, minbase rule, groupby baserule, etc.
 
 #endif RULE_H
