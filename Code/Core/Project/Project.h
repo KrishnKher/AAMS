ee4dd82af9892d20f31c19637dd1f5cab4c0c24a
Sujeeth13 <72500256+Sujeeth13@users.noreply.github.com>
2023-05-02 09:50:30 +0530

    Update Project.h

--- Code/Core/Project/Project.h
+++ Code/Core/Project/Project.h
@@ -1,25 +1,36 @@
 #include <iostream>
 #include <bits/stdc++.h>
-#include "../Rule/Rule.h"
+//#include "../Rule/Rule.h"
 #include "../SeatMatrix/SeatMatrix.h"
 using namespace std;
 
+class Rule{
+    int a;
+};
+
 class Project {
     private: 
-        Rule::Rule* currRule;
-        uint32_t currRoundState;
-        vector<uint32_t> totalRounds;
+        Rule* currRule;
+        uint currRoundState;
+        vector<uint> totalRounds;
         string baseFilePath;
         vector<string> coapResponsePaths;
+        vector<pair<string,vector<string>>> data;
+        map<pair<string,string>,vector<string>> studentPriority;
+        vector<vector<pair<string,vector<string>>>> allRoundData;
         SeatMatrix currSeatMatrix; 
     protected:
     public:
-        Project(string fileName);
+        Project(string fileName,map<string,map<string,uint>> matrix);
         // make a load file function
         void loadData(string fileName);
+        void loadStudentPriority(string fileName);
+        void deleteRow(uint id);
+        void createSeatMatrix(SeatMatrix matrix);
+        void updateSeatMatrix();
         void sort();
-        void preProcessData(uint32_t roundNumber);
-        void deleteRound(uint32_t roundNumber);
+        void preProcessData(string coapResponsePath,uint roundNumber);
+        void deleteRound(uint roundNumber);
         void addRound();
-
+        void switchRound(uint roundNumber);
 };
