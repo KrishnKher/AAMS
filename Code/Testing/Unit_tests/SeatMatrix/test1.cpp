934c6986e970b17e723f754249506dd090c7a751
Sujeeth13 <72500256+Sujeeth13@users.noreply.github.com>
2023-05-05 23:09:22 +0530

    Update test1.cpp

--- Code/Testing/Unit_tests/SeatMatrix/test1.cpp
+++ Code/Testing/Unit_tests/SeatMatrix/test1.cpp
@@ -1,1 +1,15 @@
+#include "../../../Core/SeatMatrix/SeatMatrix.h"
+using namespace std;
 
+int main(){
+    map<string,map<string,uint>> matrix;
+    matrix["hello"]["student"] = 1;
+    SeatMatrix cm(matrix);
+    SeatMatrix c = cm;
+    c.printSeats();
+    cm.printSeats();
+    cm.addSeats("hello","student",2);
+    cm.printSeats();
+    cm.deleteSeats("hello","student",1);
+    cm.printSeats();
+}
